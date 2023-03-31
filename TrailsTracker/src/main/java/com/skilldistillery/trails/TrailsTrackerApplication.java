f81f4b42860f1a8093a0af699e446902dd807263
kennyan11b <kennyan11b@gmail.com>
2023-03-31 15:44:00 -0700

    Create Boot REST project, connect JPA project dependency

---
+++ TrailsTracker/src/main/java/com/skilldistillery/trails/TrailsTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.trails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrailsTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrailsTrackerApplication.class, args);
+	}
+
+}
